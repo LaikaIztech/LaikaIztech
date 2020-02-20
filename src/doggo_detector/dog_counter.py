a847c6f4b66ac4f0ec2fb52c8b26e13ef7f895c8
ambertide <egeemirozkan24@gmail.com>
2020-02-21 01:14:51 +0300

    Fixed a lot of reduent stuff

--- src/doggo_detector/dog_counter.py
+++ src/doggo_detector/dog_counter.py
@@ -1,22 +1,30 @@
 import cv2
 import matplotlib.pyplot as plt
+from nms.nms import boxes
 
-def count_doggo(img_path: str, i: int, fig) -> int:
+def count_doggo(img_path: str) -> int:
     face_cascade = cv2.CascadeClassifier(r'..\data\catface.xml')
     img = cv2.imread(img_path)
     grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(grayscale)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x - 10,y - 10), (x+w+20, y+h+20), (0, 0, 255), 10)
+    objs = []
+    reject_levels = []
+    level_weights = []
+    objs, rej, scores = face_cascade.detectMultiScale3(grayscale, outputRejectLevels=True)
+    indices = boxes([[x, y, x + w, y + h] for (x, y,w, h) in objs], scores)
+    detections = [objs[i] for i in indices]
+    scores = [scores[i] for i in indices]
+    i = 0
+    for (x, y, w, h) in detections:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 10)
+        cv2.putText(img, f"{scores[i]}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, (36, 255, 12), 4)
+        i += 1
     cv_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    fig.add_subplot(2, 2, i + 1)
     plt.imshow(cv_rgb)
-    return len(faces)
+    return len(detections)
 
 
 if __name__ == "__main__":
     fig = plt.figure()
     fig.suptitle("Project Laika")
-    for i in range(4):
-        print(f"Number of cats: {count_doggo(f'../data/cat{i}.jpeg', i, fig)}")
+    print(f"Number of cats: {count_doggo('../data/cat0.jpeg')}")
     plt.savefig("fig.png")
